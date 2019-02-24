d14fc6f6deab4d31d13cc4d1a6e386c086dcadb5
Ryohei Ueda <garaemon@gmail.com>
2019-02-24 15:17:22 +0900

    Update

--- pr_style_review/linter_result.py
+++ pr_style_review/linter_result.py
@@ -4,3 +4,9 @@ from collections import namedtuple
 LinterResult = namedtuple(
     'LinterResult', ['level', 'line_number', 'message', 'linter_name',
                      'file_name'])
+
+
+def pretty_print_linter_result(result_array):
+    for result in result_array:
+        print('{}:L{} {}'.format(result.file_name, result.line_number,
+                                 result.message))
---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+setup(
+    name='pr_style_review',
+    version='0.0.0',
+    install_requires=['GitPython', 'github3.py'])
